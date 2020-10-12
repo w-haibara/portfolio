9b28f587fd17be1c6cf9607c153d186546df3279
github workflow <hwhaibarawataru@gmail.com>
2020-10-12 14:29:13 +0900

    Add contents of Works page

--- src/views/Works.vue
+++ src/views/Works.vue
@@ -2,39 +2,44 @@
 <template>
   <v-sheet class="blue-grey--text text--darken-4" height="100%" tile>
     <v-container>
+      <h1 class="font-weight-medium">Works</h1>
       <v-layout wrap>
-        <v-flex>
-          <h1 class="font-weight-medium">Works</h1>
-          <v-row justify="center">
-            <v-col v-for="(item, i) in items" :key="i">
-              <v-hover>
-                <template v-slot="{ hover }">
-                  <v-card
-                    :href="item.link"
-                    target="_blank"
-                    rel="noopener"
-                    :class="`elevation-${hover ? 4 : 0}`"
-                    class="transition-swing"
-                    flat
-                    tile
-                    color="grey lighten-5"
-                    min-width="40vh"
-                  >
-                    <v-card-title>
+        <v-flex v-for="(item, i) in items" :key="i">
+          <v-hover>
+            <template v-slot="{ hover }">
+              <v-card
+                :class="`elevation-${hover ? 4 : 0}`"
+                class="transition-swing my-1"
+                flat
+                tile
+                color="grey lighten-4"
+                width="42vh"
+              >
+                <v-card-title>
+                  <span>
+                    <a :href="item.link" target="_blank" rel="noopener">
                       {{ item.title }}
                       <v-icon color="blue-grey darken-5" x-small right>
                         fas fa-external-link-alt
                       </v-icon>
-                    </v-card-title>
-                    <v-card-subtitle>
-                      {{ item.subtitle }}
-                    </v-card-subtitle>
-                    <v-card-text> {{ item.text }} </v-card-text>
-                  </v-card>
-                </template>
-              </v-hover>
-            </v-col>
-          </v-row>
+                    </a>
+                  </span>
+                </v-card-title>
+                <v-card-subtitle>
+                  {{ item.subtitle }}
+                </v-card-subtitle>
+                <v-card-text>
+                  <v-row v-if="item.dev" no-gutters>
+                    <v-icon color="blue-grey darken-5" x-small left>
+                      mdi-wrench
+                    </v-icon>
+                    <span>開発中</span>
+                  </v-row>
+                  {{ item.text }}
+                </v-card-text>
+              </v-card>
+            </template>
+          </v-hover>
         </v-flex>
       </v-layout>
     </v-container>
@@ -46,45 +51,97 @@ export default {
   data: () => ({
     items: [
       {
-        title: "repo1",
-        subtitle: "aaa",
-        text: "aaaaaaaaa",
-        link: "https://github.com/w-haibara/portfolio/",
-      },
-      {
-        title: "repo2",
-        subtitle:
-          "bbb---------------------------------------------------------------------------------------------------------------------------------------------",
-        text: "bbbbbbbbb",
-        link: "https://github.com/w-haibara/portfolio/",
-      },
-      {
-        title: "repo3",
-        subtitle: "ccc",
+        title: "gelato",
+        subtitle: "Go / Raspberry Pi",
         text:
-          "ccccccccc--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------",
-        link: "https://github.com/w-haibara/portfolio/",
+          "おうちのRaspberry Pi をブラウザのみで管理できるようにする、Goアプリケーション。現在はWebコンソールでの操作が可能。リモートデスクトップ機能やセキュリティ機構を開発中。",
+        link: "https://github.com/w-haibara/gelato",
+        dev: true,
       },
       {
-        title: "repo4",
-        subtitle: "aaa",
-        text: "aaaaaaaaa",
-        link: "https://github.com/w-haibara/portfolio/",
-      },
-      {
-        title: "repo5",
-        subtitle: "bbb-----------------------------------------------",
-        text: "bbbbbbbbb",
-        link: "https://github.com/w-haibara/portfolio/",
-      },
-      {
-        title: "repo6",
-        subtitle: "ccc",
+        title: "jelly",
+        subtitle: "Go / Slack App / CI/CD",
         text:
-          "ccccccccc--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------",
-        link: "https://github.com/w-haibara/portfolio/",
+          "デプロイ自動化のためのSlack App。このページもこれによってデプロイされている。",
+        link: "https://github.com/w-haibara/jelly",
+      },
+      {
+        title: "yohkan",
+        subtitle: "GO / Ray Tracing",
+        text:
+          "Go言語製レイトレーサー。Phongの反射モデルが実装済み。影や完全鏡面反射のレンダラを開発中。",
+        link: "https://github.com/w-haibara/yohkan",
+        dev: true,
+      },
+      {
+        title: "vanilla",
+        subtitle: "Go / HTTP / AES",
+        text:
+          "イントラネットのようなHTTPSを使いづらい環境に向けた、Go言語のL7暗号化ライブラリ。クライアント側のJSライブラリを開発中。",
+        link: "https://github.com/w-haibara/vanilla",
+        dev: true,
+      },
+      {
+        title: "vuewat",
+        subtitle: "WebAssembly / Vue.js",
+        text:
+          "フルクライアントサイドのWasm Text Format to Wasm コンパイラ。現在は、関数名・引数・戻り値が固定された上での引数同士の四則演算が可能。",
+        link: "https://github.com/w-haibara/vuewat",
+        dev: true,
+      },
+      {
+        title: "potsuri",
+        subtitle: "Vue.js / SNS",
+        text: "一人用SNS。",
+        link: "https://github.com/w-haibara/potsuri",
+        dev: false,
+      },
+      {
+        title: "vtd4",
+        subtitle: "Vue.js / 4bit CPU",
+        text: "4bit CPU: TD4 のVue.js 製エミュレータ。",
+        link: "https://github.com/w-haibara/vtd4",
+        dev: false,
+      },
+      {
+        title: "syshello",
+        subtitle: "Linux system call / C / ShellScript",
+        text: "Linuxのシステムコールの開発を楽にするためのシェルスクリプト。",
+        link: "https://github.com/w-haibara/syshello",
+        dev: false,
+      },
+      {
+        title: "caes",
+        subtitle: "C / AES",
+        text: "AES-128-CTR のC言語フルスクラッチ実装。",
+        link: "https://github.com/w-haibara/caes",
+        dev: false,
+      },
+      {
+        title: "portfolio",
+        subtitle: "Vue.js / SPA",
+        text: "ご覧いただいている、このポートフォリオ。",
+        link: "https://github.com/w-haibara/portfolio",
+        dev: false,
       },
     ],
   }),
 };
 </script>
+
+<style scoped>
+span a {
+  color: #546e7a;
+  text-decoration: none;
+  display: inline-block;
+  transition: 0.3s;
+  -webkit-transform: scale(1);
+  transform: scale(1);
+}
+
+span a:hover {
+  color: black;
+  -webkit-transform: scale(1.05);
+  transform: scale(1.05);
+}
+</style>

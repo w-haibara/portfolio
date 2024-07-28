e01f35bb8065c03ba7f1673db7cee66d62a7fca9
Wataru Haibara <w-haibara@users.noreply.github.com>
2024-07-28 16:29:20 +0900

    fix image size spec

--- index.jsx
+++ index.jsx
@@ -27,7 +27,7 @@ export default function (data) {
       <div className="flex flex-row">
         <div className="basis-1/2">
           <img
-            className="h-64 aspect-square"
+            className="aspect-square"
             src="/img/image.webp"
             alt="地元の写真。草が生い茂る線路の向こうに山々が広がっている。"
           />

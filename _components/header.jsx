a97686e13c06792e04ad021f33474ab028bca92a
Wataru Haibara <w-haibara@users.noreply.github.com>
2024-07-29 11:40:13 +0900

    show humburger menu if mobile size

--- _components/header.jsx
+++ _components/header.jsx
@@ -50,6 +50,26 @@ export default function () {
               </ul>
             </nav>
           </div>
+
+          <div className="flex flex-1 items-center justify-end md:justify-between">
+            <button className="block rounded bg-gray-100 p-2.5 text-gray-600 md:hidden">
+              <span className="sr-only">Toggle menu</span>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                strokeWidth="2"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M4 6h16M4 12h16M4 18h16"
+                />
+              </svg>
+            </button>
+          </div>
         </div>
       </header>
     </>

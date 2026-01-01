c4b1fb1a8ea1b01eaf2e57d1adfebf4ef4720c49
w-haibara <39176215+w-haibara@users.noreply.github.com>
2026-01-01 13:46:40 +0900

    Refactor pageview.js to use alpine:init event

--- scripts/pageview.js
+++ scripts/pageview.js
@@ -1,19 +1,21 @@
-(async () => {
-  const ORIGIN = "https://exe.w-haibara.com";
-
-  try {
-    await fetch(`${ORIGIN}/inc`, {
-      method: "POST",
-      mode: "no-cors",
-      keepalive: true,
-    });
-  } catch (_) {}
-
-  try {
-    const res = await fetch(`${ORIGIN}/count`, { cache: "no-store" });
-    const { count } = await res.json();
-    Alpine.store("pageview", String(count))
-  } catch (_) {
-    Alpine.store("pageview", "-")
-  }
-})();
+document.addEventListener('alpine:init', () => {
+  (async () => {
+    const ORIGIN = "https://exe.w-haibara.com";
+  
+    try {
+      await fetch(`${ORIGIN}/inc`, {
+        method: "POST",
+        mode: "no-cors",
+        keepalive: true,
+      });
+    } catch (_) {}
+  
+    try {
+      const res = await fetch(`${ORIGIN}/count`, { cache: "no-store" });
+      const { count } = await res.json();
+      Alpine.store("pageview", String(count))
+    } catch (_) {
+      Alpine.store("pageview", "-")
+    }
+  })();
+});
